e9c86aa11dedc7c6a8254b68d220149228bbc8b4
hyeyeon9037 <infinite402@gmail.com>
2024-07-31 15:04:31 +0900

    0731

---
+++ mybatis/src/main/java/com/study/mybatis/member/service/MemberService.java
@@ -0,0 +1,5 @@
+package com.study.mybatis.member.service;
+
+public interface MemberService {
+	/*public abstract*/int checkId(String userId);
+}
---
+++ mybatis/src/main/java/com/study/mybatis/member/service/MemberServiceImpI.java
@@ -0,0 +1,11 @@
+package com.study.mybatis.member.service;
+
+public class MemberServiceImpI implements MemberService {
+
+	@Override
+	public int checkId(String userId) {
+		
+		return 0;
+	}
+
+}
